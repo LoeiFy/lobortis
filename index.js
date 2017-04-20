261f40705f0729d7a918e2ec82d957bec7c2541b
LoeiFy <LoeiFy@gmail.com>
2017-04-20 20:29:33 +0800

    init

---
+++ index.js
@@ -0,0 +1,10 @@
+$(function() {
+
+    $('#fullpage').fullpage({
+      verticalCentered: true,
+      anchors: ['firstPage', 'secondPage', '3rdPage', '4thPage'],
+      sectionsColor: ['#4A6FB1', '#939FAA', '#323539'],
+      scrollOverflow: true
+    })
+
+})
