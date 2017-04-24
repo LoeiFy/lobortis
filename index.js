3294f87517798e7c866944a98a9820127845cf12
LoeiFy <LoeiFy@gmail.com>
2017-04-24 11:29:25 +0800

    nav

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ $(function() {
     verticalCentered: true,
     navigation: true,
     navigationTooltips: ['HOME', 'OUR SKILLS', 'OUR WORKS', 'CONTACT'],
-    scrollOverflow: true
+    scrollOverflow: true,
+    onLeave: function(index, next) {
+      if (next === 2 || next === 3) {
+        $('#fp-nav').addClass('black')
+      } else {
+        $('#fp-nav').removeClass('black')
+      }
+    }
   })
 })
